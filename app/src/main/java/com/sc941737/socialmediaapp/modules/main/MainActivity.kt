82750b56eec5ed8987600bbf6424489331583b22
sc941737 <sergiusz.cichosz@gmail.com>
2021-06-19 19:45:08 +0100

    Added navigation component.

--- app/src/main/java/com/sc941737/socialmediaapp/modules/main/MainActivity.kt
+++ app/src/main/java/com/sc941737/socialmediaapp/modules/main/MainActivity.kt
@@ -2,11 +2,38 @@ package com.sc941737.socialmediaapp.modules.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.sc941737.socialmediaapp.R
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import android.view.View
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val viewModel:MainViewModel by viewModels()
+    private val fragmentsWithoutToolbar:Array<Int> = arrayOf() // Add fragments that should not have a toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            val shouldHideToolbar = destination.id in fragmentsWithoutToolbar
+            toolbar.visibility = if (shouldHideToolbar) View.GONE else View.VISIBLE
+        }
+        navController.navigate(R.id.feedFragment)
     }
 }
